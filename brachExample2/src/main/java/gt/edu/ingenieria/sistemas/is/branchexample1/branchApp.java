e7eae47d9db7b4505faa4334a3534772bacb8fe5
Josué Pivaral <Josu Pivaral@MJPA>
2020-03-30 20:27:43 -0600

    Creación de la clase principal y del Main

---
+++ brachExample2/src/main/java/gt/edu/ingenieria/sistemas/is/branchexample1/branchApp.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gt.edu.ingenieria.sistemas.is.branchexample1;
+
+/**
+ *
+ * @author Josué Pivaral
+ */
+public class branchApp {
+    
+   static void main (String [] args){
+   
+       System.out.println("Hola Mundo");
+   }
+    
+    
+}
